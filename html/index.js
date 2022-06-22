7ee0aabf2f1e822e9130a3dc7e0f2a0361083b00
Kieran McWilliams <kieran.j@mcwilliams.cc>
2022-06-21 19:37:54 -0600

    Updated experiences, Updated picture and favicons, Revamped some CSS, Migrated from tab=4 to tab=2 spaces.

--- html/index.js
+++ html/index.js
@@ -2,144 +2,144 @@
 
 //Gets yPosition of element from top of entire page, using all parents
 function getPosition(element) {
-    var yPosition = 0;
+  var yPosition = 0;
 
-    while (element) {
-        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
-        element = element.offsetParent;
-    }
+  while (element) {
+    yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+    element = element.offsetParent;
+  }
 
-    return yPosition;
+  return yPosition;
 }
 
 //Event listeners for resizing navbar on scroll or resize and highlighting current sections with buttons
 window.addEventListener("scroll", function() {
-    navbarUpdates();
+  navbarUpdates();
 });
 window.addEventListener("resize", function() {
-    navbarUpdates();
+  navbarUpdates();
 });
 
 var navbarUpdates = function() {
-    //Testing showing scroll and device dimensions
-    document.getElementById("testScroll").innerHTML = window.pageYOffset;
-    document.getElementById("testWidth").innerHTML = document.body.clientWidth;
-    document.getElementById("testWidth2").innerHTML = window.innerWidth;
-    document.getElementById("testHeight").innerHTML = window.innerHeight;
+  //Testing showing scroll and device dimensions
+  document.getElementById("testScroll").innerHTML = window.pageYOffset;
+  document.getElementById("testWidth").innerHTML = document.body.clientWidth;
+  document.getElementById("testWidth2").innerHTML = window.innerWidth;
+  document.getElementById("testHeight").innerHTML = window.innerHeight;
 
-    var pageYOffset = window.pageYOffset;
+  var pageYOffset = window.pageYOffset;
 
-    var navbarItems = [
-        document.getElementById("navbar"),
-        document.getElementById("navbarContainer"),
-        document.getElementById("navbarImage"),
-        document.getElementById("navbarName"),
-        document.getElementById("navbarButtonsDiv"),
-        document.getElementById("bodyContainer"),
-    ];
-    var navbarButtons = document.getElementsByClassName("navbarButton");
+  var navbarItems = [
+    document.getElementById("navbar"),
+    document.getElementById("navbarContainer"),
+    document.getElementById("navbarImage"),
+    document.getElementById("navbarName"),
+    document.getElementById("navbarButtonsDiv"),
+    document.getElementById("bodyContainer"),
+  ];
+  var navbarButtons = document.getElementsByClassName("navbarButton");
 
-    //Add navbar buttons to array
-    navbarItems = navbarItems.concat(Array.from(navbarButtons));
+  //Add navbar buttons to array
+  navbarItems = navbarItems.concat(Array.from(navbarButtons));
 
-    // // If scrolled or always on mobile
-    // if(pageYOffset >= 10 || window.innerWidth < 825 || window.innerHeight < 416) {
+  // // If scrolled or always on mobile
+  // if(pageYOffset >= 10 || window.innerWidth < 825 || window.innerHeight < 416) {
 
-    //If scrolled
-    if(pageYOffset >= 10) {
-        for(var i = 0; i < navbarItems.length; i++) {
-            navbarItems[i].classList.add("scrolled");
-        }
+  //If scrolled
+  if(pageYOffset >= 10) {
+    for(var i = 0; i < navbarItems.length; i++) {
+      navbarItems[i].classList.add("scrolled");
     }
-    else {
-        for(var i = 0; i < navbarItems.length; i++) {
-            navbarItems[i].classList.remove("scrolled");
-        }
+  }
+  else {
+    for(var i = 0; i < navbarItems.length; i++) {
+      navbarItems[i].classList.remove("scrolled");
     }
+  }
 
-    //Both navbar and mobile navbar buttons
-    var navbarButtons = Array.from(document.getElementsByClassName("navbarButton")).concat(Array.from(document.getElementsByClassName("navbarButtonMobile")));
-    var len = navbarButtons.length;
+  //Both navbar and mobile navbar buttons
+  var navbarButtons = Array.from(document.getElementsByClassName("navbarButton")).concat(Array.from(document.getElementsByClassName("navbarButtonMobile")));
+  var len = navbarButtons.length;
 
-    for(var i = 0; i < len; i++) {
-        if(!navbarButtons[i].classList.contains("icon")) {
-            var thisJumpOffset = getPosition(document.getElementById(navbarButtons[i].innerHTML.toLowerCase()));
+  for(var i = 0; i < len; i++) {
+    if(!navbarButtons[i].classList.contains("icon")) {
+      var thisJumpOffset = getPosition(document.getElementById(navbarButtons[i].innerHTML.toLowerCase()));
 
-            //Last one of navbar or mobile navbar will not have another div after it
-            if(i != len - 3 && i != (len / 2) - 3) {
-                var nextJumpOffset = getPosition(document.getElementById(navbarButtons[i + 1].innerHTML.toLowerCase()));
+      //Last one of navbar or mobile navbar will not have another div after it
+      if(i != len - 3 && i != (len / 2) - 3) {
+        var nextJumpOffset = getPosition(document.getElementById(navbarButtons[i + 1].innerHTML.toLowerCase()));
 
-                if(thisJumpOffset < pageYOffset + 25 && pageYOffset < nextJumpOffset - 25) {
-                    //If mobile navbar button give to parent
-                    if(navbarButtons[i].classList.contains("navbarButtonMobile"))
-                        navbarButtons[i].parentElement.classList.add("active");
-                    else {
-                        navbarButtons[i].classList.add("active");
-                    }
-                }
-                else {
-                    //If mobile navbar button give to parent
-                    if(navbarButtons[i].classList.contains("navbarButtonMobile"))
-                        navbarButtons[i].parentElement.classList.remove("active");
-                    else {
-                        navbarButtons[i].classList.remove("active");
-                    }
-                }
-            }
-            else {
-                if(thisJumpOffset < pageYOffset + 25) {
-                    //If mobile navbar button give to parent
-                    if(navbarButtons[i].classList.contains("navbarButtonMobile"))
-                        navbarButtons[i].parentElement.classList.add("active");
-                    else {
-                        navbarButtons[i].classList.add("active");
-                    }
-                }
-                else {
-                    //If mobile navbar button give to parent
-                    if(navbarButtons[i].classList.contains("navbarButtonMobile")) {
-                        navbarButtons[i].parentElement.classList.remove("active");
-                    }
-                    else {
-                        navbarButtons[i].classList.remove("active");
-                    }
-                }
-            }
+        if(thisJumpOffset < pageYOffset + 25 && pageYOffset < nextJumpOffset - 25) {
+          //If mobile navbar button give to parent
+          if(navbarButtons[i].classList.contains("navbarButtonMobile"))
+            navbarButtons[i].parentElement.classList.add("active");
+          else {
+            navbarButtons[i].classList.add("active");
+          }
         }
+        else {
+          //If mobile navbar button give to parent
+          if(navbarButtons[i].classList.contains("navbarButtonMobile"))
+            navbarButtons[i].parentElement.classList.remove("active");
+          else {
+            navbarButtons[i].classList.remove("active");
+          }
+        }
+      }
+      else {
+        if(thisJumpOffset < pageYOffset + 25) {
+          //If mobile navbar button give to parent
+          if(navbarButtons[i].classList.contains("navbarButtonMobile"))
+            navbarButtons[i].parentElement.classList.add("active");
+          else {
+            navbarButtons[i].classList.add("active");
+          }
+        }
+        else {
+          //If mobile navbar button give to parent
+          if(navbarButtons[i].classList.contains("navbarButtonMobile")) {
+            navbarButtons[i].parentElement.classList.remove("active");
+          }
+          else {
+            navbarButtons[i].classList.remove("active");
+          }
+        }
+      }
     }
+  }
 }
 
 //Run at startup
 $(document).ready(function() {
-    navbarUpdates();
+  navbarUpdates();
 
-    var mobileNavbarMenu = document.getElementById("mobileNavbarMenu");
+  var mobileNavbarMenu = document.getElementById("mobileNavbarMenu");
 
-    mobileNavbarMenu.addEventListener("click", function() {
-        var mobileNavbar = document.getElementById("mobileNavbar");
+  mobileNavbarMenu.addEventListener("click", function() {
+    var mobileNavbar = document.getElementById("mobileNavbar");
 
-        if(mobileNavbar.classList.contains("open")) {
-            mobileNavbar.classList.toggle("open");
-            mobileNavbar.style.height = "0px";
+    if(mobileNavbar.classList.contains("open")) {
+      mobileNavbar.classList.toggle("open");
+      mobileNavbar.style.height = "0px";
 
-            // document.getElementById("navbar").classList.remove("scrolled");
-        }
-        else {
-            mobileNavbar.classList.toggle("open");
-            mobileNavbar.style.height = mobileNavbar.scrollHeight + "px";
+      // document.getElementById("navbar").classList.remove("scrolled");
+    }
+    else {
+      mobileNavbar.classList.toggle("open");
+      mobileNavbar.style.height = mobileNavbar.scrollHeight + "px";
 
-            document.getElementById("navbar").classList.add("scrolled");
-        }
-    });
+      document.getElementById("navbar").classList.add("scrolled");
+    }
+  });
 
-    window.addEventListener("click", function(event) {
-        if(event.target.id != "mobileNavbarMenu") {
-            var mobileNavbar = document.getElementById("mobileNavbar");
+  window.addEventListener("click", function(event) {
+    if(event.target.id != "mobileNavbarMenu") {
+      var mobileNavbar = document.getElementById("mobileNavbar");
 
-            mobileNavbar.classList.remove("open");
-            mobileNavbar.style.height = "0px";
+      mobileNavbar.classList.remove("open");
+      mobileNavbar.style.height = "0px";
 
-            // document.getElementById("navbar").classList.remove("scrolled");
-        }
-    });
+      // document.getElementById("navbar").classList.remove("scrolled");
+    }
+  });
 });
